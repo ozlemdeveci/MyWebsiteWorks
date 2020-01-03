5990f05cc7efe1e4d6e2d020e64353c920338a5b
ozlemdeveci <ozlemelisss@gmail.com>
2020-01-03 17:56:27 -0600

    my website works

---
+++ com/FaceBookDate.java
@@ -0,0 +1,14 @@
+package website.com;
+
+public class FaceBookDate {
+	/*open chrome browser
+	 * go to http://www.facebook.com"
+	 * Verify:
+	 * month dd has 12 month options
+	 * day dd has 31 day
+	 * year dd has 115 year options
+	 * select your date of birth
+	 * quit browser
+	 */
+
+}
---
+++ com/Toolsqa.java
@@ -0,0 +1,19 @@
+package website.com;
+
+public class Toolsqa {
+	/*open chrome browser
+	 * go to "http://www.toolsqa.com/automation-practice-form/"
+	 * fill out
+	 * first name, lastname,
+	 * check all years of experience radio buttons are clickable
+	 * date
+	 * select both checkboxes of proffecsions
+	 * check all Automation Tools checkboxes are clickable
+	 * and keep"selenium webDriver" option as selected
+	 * quit browser
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
